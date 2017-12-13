3216901ffa1f15f6f0023ac4e9be0971582de7e1
hmainismael <hmain.ismael@hotmail.fr>
2017-12-13 14:29:57 +0100

    Creation page d'accueil

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Projet M1 I2L - Jeu TINTAS</title>
+
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="css/freelancer.min.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <nav class="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">DIETER STAIN</a>
+        <button class="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">Inscription</a>
+            </li>
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Connexion</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <header class="masthead bg-primary text-white text-center">
+      <div class="container">
+        <img class="img-fluid mb-5 d-block mx-auto" src="img/tintas.jpg" alt="" width="500px">
+        <h2 class="text-center text-uppercase text-white">Règles du jeu</h2>
+        <hr class="star-light mb-5">
+        <div class="row">
+          <div class="col-lg-4 ml-auto">
+            <p class="lead">But du jeu : « Le joueur qui récupère les 7 pièces de même couleur gagne le jeu »</p>
+          </div>
+          <div class="col-lg-4 mr-auto">
+            <p class="lead">Le premier joueur place le pion sur un hexagone de son choix, prend le pion qui se trouve là et le place devant lui. Cela termine son tour de jeu</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 ml-auto">
+            <p class="lead">A partir de maintenant, le joueur actif doit déplacer le pion dans l'une des six directions possibles. Le cas échéant, à travers les espaces inoccupés - jusqu'à ce qu'il atteigne un espace occupé. Il recueille la pièce de jeu se trouvant sur cet espace et place le pion là.</p>
+          </div>
+          <div class="col-lg-4 mr-auto">
+            <p class="lead">S'il peut récolter une autre pièce de la  même couleur en continuant à déplacer le pion, le joueur actif peut continuer jusqu'à ce qu'il ne soit plus capable ou désireux de collecter une autre pièce de même couleur.</p>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <div class="scroll-to-top d-lg-none position-fixed ">
+      <a class="js-scroll-trigger d-block text-center text-white rounded" href="#page-top">
+        <i class="fa fa-chevron-up"></i>
+      </a>
+    </div>
+
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="js/freelancer.min.js"></script>
+
+  </body>
+
+</html>
